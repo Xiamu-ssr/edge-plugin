86f609183cb30c909999d3e63e8ff00672b2a705
Xiamu-ssr <1138882663@qq.com>
2024-01-21 21:44:25 +0800

    支持数字，比如swagger3，sl4f2j

--- scripts/content.js
+++ scripts/content.js
@@ -150,9 +150,7 @@ function applyRule(rule, text) {
 //功能01
 function func_01(text){
     console.log("func_01 start, text=", text);
-    // text = text.replace(/([a-zA-Z]+([-_][a-zA-Z]+)*)/g, '`$1`');
-    // text = text.replace(/(?<!`)([a-zA-Z]+([-_][a-zA-Z]+)*)(?!`)/g, '`$1`');
-    text = text.replace(/(?<!`)\b([a-zA-Z]+([-\s_.][a-zA-Z]+)*)\b(?!`)/g, '`$1`');
+    text = text.replace(/(?<!`)\b([a-zA-Z0-9]+([-\s_.][a-zA-Z0-9]+)*)\b(?!`)/g, '`$1`');
     console.log("func_01 -over, text=", text);
     return text;
 }
